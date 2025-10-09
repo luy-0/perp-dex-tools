75f02329f83cea756d7f595e829e90b0e1613889
luy <seven_ou@163.com>
2025-10-09 16:07:01 +0800

    shell

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 source ./env/bin/activate
-python runbot.py --exchange aster --ticker BTC --quantity 0.001 --take-profit 0.02 --max-orders 40 --wait-time 450
-python runbot.py --exchange aster --ticker BTC --direction sell --quantity 0.001 --take-profit 0.02 --max-orders 15 --wait-time 450
+# python runbot.py --exchange aster --ticker BTC --quantity 0.001 --take-profit 0.02 --max-orders 40 --wait-time 450
+# python runbot.py --exchange aster --ticker BTC --direction sell --quantity 0.001 --take-profit 0.02 --max-orders 15 --wait-time 450
+
+python runbot.py --exchange grvt --ticker BTC --quantity 0.005 --grid-step 0.05 --take-profit 0.02 --max-orders 40 --wait-time 450
+# 121964 * (1-(40*0.03/100))
