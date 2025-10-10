88f115751094df8e174daa42469589a333ad5264
luy <seven_ou@163.com>
2025-10-10 12:57:03 +0800

    sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 source ./env/bin/activate
 # python runbot.py --exchange aster --ticker BTC --quantity 0.001 --take-profit 0.02 --max-orders 40 --wait-time 450
 # python runbot.py --exchange aster --ticker BTC --direction sell --quantity 0.001 --take-profit 0.02 --max-orders 15 --wait-time 450
-
-python runbot.py --exchange grvt --ticker BTC --quantity 0.005 --grid-step 0.25 --take-profit 0.02 --max-orders 40 --wait-time 450
+nohup python3 runbot.py --exchange grvt --ticker BTC --quantity 0.005 --grid-step 0.25 --take-profit 0.02 --max-orders 40 --wait-time 450 >out.log 2>&1 &
 # 当前价格 = 121964 
 # 每个网格间隔价格 = 121964 * (0.25/100)  # grid-step  
 # 最多持有头寸:  0.005 * 40
